a4d47f4ce0e1babe9f655af785ff90ddaf83fc89
Ritvik_Pramod <103210186+Ritvikadoor@users.noreply.github.com>
2022-09-19 19:23:35 +0530

    added images and drawer

--- lib/view/main_screen.dart
+++ lib/view/main_screen.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:ritvik_p_pramod/constants.dart';
 
 class MainScreen extends StatelessWidget {
   const MainScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Drawer(
+                  child: Column(
+                    children: [
+                      AspectRatio(
+                        aspectRatio: 1.23,
+                        child: Container(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                )),
+            Expanded(
+                flex: 7,
+                child: Container(
+                  color: Colors.blue,
+                ))
+          ],
+        ),
+      ),
+    );
   }
 }
